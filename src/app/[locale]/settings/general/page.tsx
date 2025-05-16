c50cd10b264d814df749316a0db23b2bd4b8c280
Sekai <baonguyen212002@gmail.com>
2025-05-16 16:56:47 +0700

    update locale/settings/general

--- src/app/[locale]/settings/general/page.tsx
+++ src/app/[locale]/settings/general/page.tsx
@@ -1,6 +1,5 @@
-export const dynamic = 'force-dynamic';
-
 'use client';
+export const dynamic = 'force-dynamic';
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Cog } from 'lucide-react';
