cbfdde35bda50cdae09ceb01518ffeabd914155f
Sekai <baonguyen212002@gmail.com>
2025-05-19 11:57:28 +0700

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} antialiased`}>
         {children} {/* This will render the content from [locale]/layout.tsx */}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
